b1c1255c99bafb50a48c823159a4b8926cfc1afe
sanad-bhowmik <sanadbhowmik93@gmail.com>
2023-03-31 00:41:59 +0600

    connected to font awesome

--- src/components/Author/Author.jsx
+++ src/components/Author/Author.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import './Author.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 
 const Author = (props) => {
-    const {author,title,coverImageUrl,authorImageUrl,readTime,publishDate} = props.blog;
+    const { author, title, coverImageUrl, authorImageUrl, readTime, publishDate } = props.blog;
     return (
-        <div className='author-details'>
-            <img src={coverImageUrl} alt="" />
+        <div className='author-details-container'>
+            <img src={coverImageUrl} alt="" className='coverImg'/>
+            <div className='blog-details'>
+                <div className='author-details'>
+                    <img src={authorImageUrl} alt="" />
+                    <div className='Name'>
+                        <h5>{author}</h5>
+                        <p>{publishDate}</p>
+                    </div>
+                </div>
+                <div>
+                    <p>{readTime} min read<FontAwesomeIcon icon={faBookmark} /></p>
+                </div>
+            </div>
+            <hr />
         </div>
     );
 };
