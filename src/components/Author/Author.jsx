d283ece2ab97a00ec4071d5ff5c48fae3db33d5a
sanad-bhowmik <sanadbhowmik93@gmail.com>
2023-03-31 01:27:55 +0600

    author design done

--- src/components/Author/Author.jsx
+++ src/components/Author/Author.jsx
@@ -16,10 +16,13 @@ const Author = (props) => {
                         <p>{publishDate}</p>
                     </div>
                 </div>
-                <div>
+                <div className='readingTime'>
                     <p>{readTime} min read<FontAwesomeIcon icon={faBookmark} /></p>
                 </div>
             </div>
+            <h2 className='title'>{title}</h2>
+            <p style={{color: "gray" , fontSize:"15px"}}>#beginners #programming</p>
+            <a href="">Mark as read</a>
             <hr />
         </div>
     );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css'
 
+
 const Header = () => {
     return (
         <nav className='header'>
@@ -8,7 +9,7 @@ const Header = () => {
             <div>
                 <a href="/blog">Blog</a>
                 <a href="/write">Write</a>
-                <a href="/login">Login</a>
+                <a href="/login">Log in</a>
                 <a href="/image"><img src="https://media.licdn.com/dms/image/D5603AQEOjP3ON2knRg/profile-displayphoto-shrink_800_800/0/1677507598430?e=2147483647&v=beta&t=G-6CChYSPDCnu-Hl5VBqfgSQax6aZaCfkOnQWVm_584" alt="Avatar"  className='avatar'/></a>
             </div>
         </nav>
